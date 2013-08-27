28548889d2300ad03a7c629af65e2b90fe64a637
Chris Holtz <choltz@gmail.com>
2013-08-27 09:28:10 -0400

    Updated offlineimap action to kill existing offlineimap process before kicking off a new one

--- actions/offlineimap.rb
+++ actions/offlineimap.rb
@@ -1,15 +1,20 @@
+# Public: Calls offlineimap to sync up mail box
 class Offlineimap
   def call
+    puts '*** starting offline imap synchronization'
     old_messages = messages
-    puts "*** starting offline imap synchronization"
-    system "offlineimap -u quiet" # `offlineimap -u quiet`
-    new_messages = messages
-    puts "*** imap synchronization complete"
 
-    return new_messages - old_messages
+    # Kill existing offlineimap process and start a new one
+    puts `pgrep -f 'offlineimap'`.split("\n").each{ |p| `kill -9 #{p}` }
+    system 'offlineimap -u quiet' # `offlineimap -u quiet`
+    new_messages = messages
+    puts '*** imap synchronization complete'
+
+    new_messages - old_messages
   end
 
-  def messages()
+  # Internal: Retrieve message file names
+  def messages
     Dir["#{ENV["HOME"]}/Maildir/*/*/*"]
   end
 
