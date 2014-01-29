fb26b2be4ae2bd969d2cad499b9badd7be8d734f
Chris Holtz <choltz@gmail.com>
2014-01-28 22:44:13 -0500

    Added a bit of comments to the logger class

--- actions/message_logger.rb
+++ actions/message_logger.rb
@@ -1,5 +1,13 @@
 require 'logger'
+
+# Public: Simple logger class to dump message information to log/message.log
 class MessageLogger
+  # Public: invoke the message logger
+  #   options: hash of key value pairs to write to the log file
+  #
+  #   Note:
+  #   The plan is to enhance this class to parse the message and log more thant just
+  #   the file and path.
   def call(options={})
     logger = Logger.new("log/message.log")
     logger.debug(options[:messages])
