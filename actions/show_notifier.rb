0a0b0736c99af10db57baf1ed5c1e92283957b75
Chris Holtz <choltz@gmail.com>
2014-02-02 22:17:24 -0500

    Added unit tests for the show_notifier class.

--- actions/show_notifier.rb
+++ actions/show_notifier.rb
@@ -1,6 +1,16 @@
+# Public: Display the message data in a popup notificaton
 class ShowNotifier
+  attr_accessor :prefix
+
+  def initialize
+    @prefix = "DISPLAY=:0.0 XAUTHORITY=~/.Xauthority"
+  end
+
+  # Public: Invoke this action - display message data in a notification window
+  #  options:
+  #    messages - array of message data.
   def call(options={})
     options = { :messages => [] }.update(options)
-    `DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send 'New Mail' 'You have #{options[:messages].length} unread messages.\nDebug: #{options[:messages]}' -t 4000`
+    Kernel.system "#{@prefix} notify-send 'New Mail' 'You have #{options[:messages].length} unread messages.\nDebug: #{options[:messages]}' -t 4000"
   end
 end
---
+++ spec/show_notifier_spec.rb
@@ -0,0 +1,11 @@
+require_relative "spec_helper"
+
+describe ShowNotifier do
+  it "displays a popup notification containing message data" do
+    action   = ShowNotifier.new
+    messages = ["buh1", "buh2"]
+
+    Kernel.should_receive(:system).with("#{action.prefix} notify-send 'New Mail' 'You have #{messages.length} unread messages.\nDebug: #{messages}' -t 4000")
+    action.call :messages => messages
+  end
+end
